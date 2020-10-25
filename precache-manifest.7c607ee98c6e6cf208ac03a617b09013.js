cf6a3d6db959dac895e0b1f2ee62fa12b8dc9fe5
mhsss <tostau123@gamil.com>
2020-10-26 01:21:21 +0200

    Updates

---
+++ precache-manifest.7c607ee98c6e6cf208ac03a617b09013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e176e02592b2efb0577a46e342bb7807",
+    "url": "/gitHubPages/index.html"
+  },
+  {
+    "revision": "c296eb8733277b35ef63",
+    "url": "/gitHubPages/static/css/main.67ab5835.chunk.css"
+  },
+  {
+    "revision": "8e7b06d79f077551f10d",
+    "url": "/gitHubPages/static/js/2.ce252236.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gitHubPages/static/js/2.ce252236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c296eb8733277b35ef63",
+    "url": "/gitHubPages/static/js/main.792918d3.chunk.js"
+  },
+  {
+    "revision": "ef2615deab89b9c67930",
+    "url": "/gitHubPages/static/js/runtime-main.8b1a3f62.js"
+  }
+]);
